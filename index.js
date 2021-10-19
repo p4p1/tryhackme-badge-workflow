ccdf0b2500d643b840e493d1b76f6587c6a7c87b
p4p1 <p4p1@vivaldi.net>
2021-10-19 15:09:28 +0100

    download working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const dlImg = (async (githubToken, filePath, username) => {
+  const url = `https://tryhackme-badges.s3.amazonaws.com/${username}.png`;
+  const path = filePath;
+
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+});
+
 console.log('hello World');
+dlImg('aa', './assets/thm_propic.png', 'p4p1');
