942869ef56ee18d790e6c299022dfa033a7db6c3
p4p1 <p4p1@vivaldi.net>
2021-10-19 14:45:14 +0100

    testing action

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+console.log('hello World');
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
